7c27ec075763821bee8f0f0e27b370cf70d955bd
Alex Gitari <giarimurithialex@gmail.com>
2023-09-05 20:03:51 +0300

    rerun pipeline

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const compression = require("compression");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const app = express();
@@ -7,6 +9,8 @@ const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression());
+app.use(helmet())
 
 app.listen(port, () => {
   console.info(`Server is running on http://localhost:${port}`);
