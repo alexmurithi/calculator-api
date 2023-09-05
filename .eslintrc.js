1d4bfad93c1920e38c4aa0ceb8248f3f2f8bad1b
Alex Gitari <giarimurithialex@gmail.com>
2023-09-05 22:35:10 +0300

    configure lint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:node/recommended'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+}
--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const compression = require("compression");
-const helmet = require("helmet");
-require("dotenv").config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const compression = require('compression')
+const helmet = require('helmet')
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 8000;
+const app = express()
+const port = process.env.PORT || 8000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(compression())
 app.use(helmet())
 
 app.listen(port, () => {
-  console.info(`Server is running on http://localhost:${port}`);
-});
+  console.log(`Server is running on http://localhost:${port}`)
+})
