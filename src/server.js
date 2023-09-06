faa2bd0becbade14f4bc2bfc96473242943d23a1
Alex Gitari <giarimurithialex@gmail.com>
2023-09-06 16:35:23 +0300

    swagger documentation

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
 const helmet = require('helmet')
+const swaggerUI = require('swagger-ui-express')
+
+const YAML = require('yamljs')
+
 const router = require('./controllers/calculation.controller')
+
 require('./config/db.config')
+
+const swaggerDocument = YAML.load('./openapi.yaml')
 
 const app = express()
 const port = process.env.PORT || 8000
@@ -15,6 +22,7 @@ app.use(compression())
 app.use(helmet())
 
 app.use('/api/calculation', router)
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
